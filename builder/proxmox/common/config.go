931e7cd4c695c470dd452c2cf25732261f78f28f
Steve Shoecraft <spshoecraft@gmail.com>
2026-03-11 12:16:46 -0500

    Add skip_convert_to_template option (default: true)
    - Add skip_convert_to_template config option to skip VM-to-template conversion
    - Default to true so the plugin produces a VM instead of a template
    - Set to false to get the original template conversion behavior
    - Handle running VM state when force-deleting existing artifacts
    - Rename stepFinalizeTemplateConfig to stepFinalizeConfig
    - Based on PR #283 by mpywell with default flipped to true

--- builder/proxmox/common/config.go
+++ builder/proxmox/common/config.go
@@ -178,6 +178,9 @@ type Config struct {
 	// Description of the template, visible in
 	// the Proxmox interface.
 	TemplateDescription string `mapstructure:"template_description"`
+	// Skip converting the VM to a template on completion of build.
+	// Defaults to `true`
+	SkipConvertToTemplate bool `mapstructure:"skip_convert_to_template"`
 
 	// If true, add an empty Cloud-Init CDROM drive after the virtual
 	// machine has been converted to a template. Defaults to `false`.
@@ -582,6 +585,8 @@ type pciDeviceConfig struct {
 func (c *Config) Prepare(upper interface{}, raws ...interface{}) ([]string, []string, error) {
 	// Do not add a cloud-init cdrom by default
 	c.CloudInit = false
+	// Skip converting to template by default
+	c.SkipConvertToTemplate = true
 	var md mapstructure.Metadata
 	err := config.Decode(upper, &config.DecodeOpts{
 		Metadata:           &md,
